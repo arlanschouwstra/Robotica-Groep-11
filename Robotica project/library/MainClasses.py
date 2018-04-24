e5d0515f589d40f2726335e16af2ff041732fa95
arlanschouwstra <scho1514@student.nhl.nl>
2018-04-24 11:03:17 +0200

    solved langauge errors

--- Robotica project/library/MainClasses.py
+++ Robotica project/library/MainClasses.py
@@ -4,67 +4,92 @@ class Servo:
 
     def rotate(self):
         pass
+
     def forward(self):
         pass
+
     def backwards(self):
         pass
+
 
 class Network:
     def __init__(self):
         pass
+
     def configure(self):
         pass
+
 
 class BluetoothController:
     def __init__(self):
         pass
+
     def configure(self):
         pass
+
 
 class Leg:
     def __init__(self):
         pass
+
     def moveForward(self):
         self
+
     def moveBackwards(self):
         pass
+
     def rotate(self):
         pass
+
 
 class Lights:
     def __init__(self):
         pass
+
     def turnOn(self):
         pass
+
     def turnOff(self):
         pass
+
 
 class Camera:
     def __init__(self):
         pass
+
     def turnOn(self):
         pass
+
     def turnOff(self):
         pass
+
     def reset(self):
         pass
+
     def detectObjects(self):
         pass
+
 
 class Dance:
     def __init__(self):
         pass
+
     def configureLegs(self):
         pass
+
 
 class Arm:
     def __int__(self):
         pass
+
     def grab(self):
         pass
+
     def rotate(self):
         pass
+
     def moveForward(self):
         pass
+
     def moveBackwards(self):
         pass
