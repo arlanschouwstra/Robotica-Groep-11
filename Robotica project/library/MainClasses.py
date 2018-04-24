d2cfb448e82be3c9e38ec4b8f45368252e318e6a
arlanschouwstra <scho1514@student.nhl.nl>
2018-04-24 10:37:21 +0200

    added standard classes

---
+++ Robotica project/library/MainClasses.py
@@ -0,0 +1,80 @@
+class Servo{
+
+	def rotate{
+	}
+
+	def forward{
+	}
+
+	def backwards{
+	}
+}
+
+class Network{
+	
+	def configure{
+	}
+}
+
+class BluetoothController{
+
+	def configure{
+	}
+}
+
+class Leg{
+
+	def moveForward{
+	}
+	
+	def moveBackwards{
+	}
+	
+	def rotate{
+	}
+}
+
+class Lights{
+	
+	def turnOn{
+	}
+	
+	def turnOff{
+	}
+}
+
+class Camera{
+	
+	def turnOn{
+	}
+	
+	def turnOff{
+	}
+	
+	def reset{
+	}
+	
+	def detectObjects{
+	}
+}
+
+class Dance{
+
+	def configureLegs{
+	}
+}
+
+class Arm{
+
+	def grab{
+	}
+	
+	def rotate{
+	}
+	
+	def moveForward{
+	}
+	
+	def moveBackwards{
+	}
+}
