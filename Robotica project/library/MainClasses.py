5e724f542c5424ce41b1d84804ad3dd815dd03a9
Thijs <thetcr@live.nl>
2018-04-24 14:54:00 +0200

    servo setup

--- Robotica project/library/MainClasses.py
+++ Robotica project/library/MainClasses.py
@@ -1,5 +1,18 @@
 class Servo:
-    def __init__(self):
+    import RPi.GPIO as GPIO
+
+    def __init__(self, pin):
+
+        # define the pin for the servo
+        self.pin = pin
+
+        # setup the pin for output
+        GPIO.setup(pin, GPIO.OUT)
+        pass
+
+    # move the servo to a given position
+    def move(self, position):
+        GPIO.output(self.pin, position)
         pass
 
     def rotate(self):
