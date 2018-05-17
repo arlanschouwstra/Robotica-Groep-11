9451200842dc0f223cb4f74245c88ae3338db573
arlanschouwstra <scho1514@student.nhl.nl>
2018-05-17 14:32:14 +0200

    simplified servo code

--- Models/Servo.py
+++ Models/Servo.py
@@ -1,6 +1,5 @@
 import pygame
 import ax12 as x
-import time
 
 
 class Servo:
@@ -9,24 +8,23 @@ class Servo:
         pygame.init()
         pygame.display.set_mode()
 
-    def move_forward(self, id):                     # moving the servo forward on button press
+    def move(self, id):                     # moving the given servo forward or backward on button press
         y = x.Ax12()
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:    # can be changed later for controller keys or joysticks
                         y.moveSpeed(id, 0, 1000)
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:    # can be changed later for controller keys or joysticks
-                        y.moveSpeed(id, 0, 0)
-
-    def move_backward(self, id):                    # moving the servo backward on button press
-        y = x.Ax12()
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_DOWN:  # can be changed later for controller keys or joysticks
                         y.moveSpeed(id, 0, 2000)
                 if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:    # can be changed later for controller keys or joysticks
+                        y.moveSpeed(id, 0, 0)
                     if event.key == pygame.K_DOWN:  # can be changed later for controller keys or joysticks
                         y.moveSpeed(id, 0, 0)
+
+
+# call method for class:
+servo = Servo()
+
+servo.move(17)
