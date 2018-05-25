3bfd14da9db964a37b7896df1c8e74affab381f6
hoog1511 <larshoogma@gmail.com>
2018-05-25 12:42:44 +0200

    updated camera.py
    crashes a lot less now.

--- Models/Camera.py
+++ Models/Camera.py
@@ -1,5 +1,9 @@
-import cv2
+# import the necessary packages
+from collections import deque
 import numpy as np
+import argparse
+import imutils
+import cv2
 import time
 
 
@@ -12,16 +16,17 @@ class Camera:
         pass
 
     def detect(self):
-        FRAME_WIDTH = 320
-        FRAME_HEIGHT = 240
+        # construct the argument parse and parse the arguments
+        ap = argparse.ArgumentParser()
+        ap.add_argument("-v", "--video",
+                        help="path to the (optional) video file")
+        ap.add_argument("-b", "--buffer", type=int, default=32,
+                        help="max buffer size")
+        args = vars(ap.parse_args())
+        # define the lower and upper boundaries of the "green" ball in the HSV color space (HUE SATURATION VALUE)
 
         # Initial HSV GUI slider values to load on program start.
-        # icol = (36, 202, 59, 71, 255, 255)    # Green
-        # icol = (18, 0, 196, 36, 255, 255)  # Yellow
-        # icol = (89, 0, 0, 125, 255, 255)  # Blue
-        # icol = (0, 100, 80, 10, 255, 255)   # Red
-        # icol = (104, 117, 222, 121, 255, 255)   # test
-        icol = (0, 105, 165, 255, 255, 255)  # New start
+        icol = (0, 0, 0, 255, 255, 255)  # start Value low/high
 
         cv2.namedWindow('colorTest')
         # Lower range colour sliders.
@@ -32,14 +37,28 @@ class Camera:
         cv2.createTrackbar('high_hue', 'colorTest', icol[3], 255, self.nothing)
         cv2.createTrackbar('high_sat', 'colorTest', icol[4], 255, self.nothing)
         cv2.createTrackbar('high_val', 'colorTest', icol[5], 255, self.nothing)
+        # redLowerHSV = (170, 200, 50)
+        # redUpperHSV = (255, 255, 255)
 
-        # Initialize webcam. Webcam 0 or webcam 1 or ...
-        vidCapture = cv2.VideoCapture(0)
-        vidCapture.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
-        vidCapture.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
+        # initialize the list of tracked points, the frame counter, and the coordinate deltas
+        pts = deque(maxlen=args["buffer"])
+        counter = 0
+        (dX, dY) = (0, 0)
+        direction = ""
 
+        # grab the reference to the webcam
+        camera = cv2.VideoCapture(0)
+
+        # keep looping
         while True:
-            time_check = time.time()
+            # grab the current frame
+            (_, frame) = camera.read()
+
+            # resize the frame, blur it, and convert it to the HSV color space
+            frame = imutils.resize(frame, width=600)
+            blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
             # Get HSV values from the GUI sliders.
             low_hue = cv2.getTrackbarPos('low_hue', 'colorTest')
             low_sat = cv2.getTrackbarPos('low_sat', 'colorTest')
@@ -48,49 +67,82 @@ class Camera:
             high_sat = cv2.getTrackbarPos('high_sat', 'colorTest')
             high_val = cv2.getTrackbarPos('high_val', 'colorTest')
 
-            # Get webcam frame
-            _, frame = vidCapture.read()
-
-            # Show the original image.
-            cv2.imshow('frame', frame)
-
-            # Convert the frame to HSV colour model.
-            frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            # HSV values to define a colour range we want to create a mask from.
+            # construct a mask for the color "green", then perform a series of dilations and erosions to remove any small blobs left in the mask
             color_low = np.array([low_hue, low_sat, low_val])
             color_high = np.array([high_hue, high_sat, high_val])
-            mask = cv2.inRange(frame_hsv, color_low, color_high)
-            # Show the first mask
-            cv2.imshow('mask-plain', mask)
+            mask = cv2.inRange(hsv, color_low, color_high)
+            mask = cv2.erode(mask, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
 
-            im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            # find contours in the mask and initialize the current (x, y) center of the ball
+            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)[-2]
+            center = None
+            # only proceed if at least one contour was found
+            if len(cnts) > 0:
+                # find the largest contour in the mask, then use it to compute the minimum enclosing circle and centroid
+                contour_sizes = [(cv2.contourArea(contour), contour) for contour in cnts]
+                c = max(contour_sizes, key=lambda x: x[0])[1]
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-            contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
-            biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
+                # only proceed if the radius meets a minimum size
+                if radius > 10:
+                    # draw the circle and centroid on the frame, then update the list of tracked points
+                    cv2.circle(frame, (int(x), int(y)), int(radius),
+                               (0, 255, 255), 2)
+                    cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                    pts.appendleft(center)
+            # loop over the set of tracked points
+            for i in np.arange(1, len(pts)):
+                # if either of the tracked points are None, ignore them
+                if pts[i - 1] is None or pts[i] is None:
+                    continue
 
-            # cv2.drawContours(frame, biggest_contour, -1, (0,255,0), 3)
+                # check to see if enough points have been accumulated in the buffer
+                if counter >= 10 and i == 1 and pts[-10] is not None:
+                    # compute the difference between the x and y coordinates and re-initialize the direction text variables
+                    dX = pts[-10][0] - pts[i][0]
+                    dY = pts[-10][1] - pts[i][1]
+                    (dirX, dirY) = ("", "")
 
-            x, y, w, h = cv2.boundingRect(biggest_contour)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    # ensure there is significant movement in the x-direction
+                    if np.abs(dX) > 20:
+                        dirX = "East" if np.sign(dX) == 1 else "West"
 
-            cv2.drawContours(frame, contours, -1, (255, 255, 0), 3)
+                    # ensure there is significant movement in the y-direction
+                    if np.abs(dY) > 20:
+                        dirY = "North" if np.sign(dY) == 1 else "South"
 
-            # cv2.drawContours(frame, contours, 3, (0,255,0), 3)
+                    # handle when both directions are non-empty
+                    if dirX != "" and dirY != "":
+                        direction = "{}-{}".format(dirY, dirX)
 
-            # cnt = contours[1]
-            # cv2.drawContours(frame, [cnt], 0, (0,255,0), 3)
+                    # otherwise, only one direction is non-empty
+                    else:
+                        direction = dirX if dirX != "" else dirY
 
-            # Show final output image
-            cv2.imshow('colorTest', frame)
+            # show the movement deltas and the direction of movement on the frame
+            cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.65, (0, 0, 255), 3)
+            cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
+                        (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.35, (0, 0, 255), 1)
 
-            k = cv2.waitKey(5) & 0xFF
-            if k == 27:
+            # show the frame to our screen and increment the frame counter
+            cv2.imshow("Frame", frame)
+            cv2.imshow("Mask", mask)
+            key = cv2.waitKey(1) & 0xFF
+            counter += 1
+
+            # if the 'q' key is pressed, stop the loop
+            if key == ord("q"):
                 break
-            print('fps - ', 1 / (time.time() - time_check))
 
+        # cleanup the camera and close any open windows
+        camera.release()
         cv2.destroyAllWindows()
-        vidCapture.release()
 
 
 # calling method:
