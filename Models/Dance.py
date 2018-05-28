35e963275ee2ae93965b589d1f3458d47b934bb6
hoog1511 <larshoogma@gmail.com>
2018-05-28 14:20:56 +0200

    updated dance

--- Models/Dance.py
+++ Models/Dance.py
@@ -10,30 +10,34 @@ class Dance:
         # set servos to their start positions
         y.moveSpeed(15, 823, 50)
         y.moveSpeed(3, 200, 50)
-        y.moveSpeed(23, 823, 50)
+        y.moveSpeed(23, 623, 50)
         y.moveSpeed(41, 512, 50)
         y.moveSpeed(51, 823, 50)
         y.moveSpeed(6, 512, 50)
 
-    def twist(y):
+    def twist(self, y):
         y.moveSpeed(6, 412, 100)    # turn far left
         time.sleep(1)
         y.moveSpeed(6, 612, 200)    # turn far right
         time.sleep(1)
-        y.moveSpeed(6, 512, 100)    # turn back to start point
-        time.sleep(1)
+        # y.moveSpeed(6, 512, 100)    # turn back to start point
+        # time.sleep(1)
 
-    def stretchForward(y):
+    def stretchForward(self, y):
         y.moveSpeed(3, 0, 200)
         y.moveSpeed(15, 1023, 200)
         y.moveSpeed(23, 823, 200)
-        time.sleep(1)
 
-    def stretchBackward(y):
+    def stretchBackward(self, y):
         y.moveSpeed(3, 200, 200)
         y.moveSpeed(15, 823, 200)
         y.moveSpeed(23, 623, 200)
-        time.sleep(1)
+
+    def moveLeft(self, y):
+        y.moveSpeed(41, 400, 200)
+
+    def moveRight(self, y):
+        y.moveSpeed(41, 600, 200)
 
     def start(self):
         self.set_start_position()
@@ -52,15 +56,15 @@ class Dance:
         y = x.Ax12()
 
         # LETS DO THE TWIST
-        for _ in xrange(10):
+        doTwist = True
+        while(doTwist == True):
             self.twist(y)
-            time.sleep(3)
-            self.stretchForward(y)
-            time.sleep(1)
-            self.twist(y)
-            time.sleep(3)
-            self.stretchBackward(y)
-            time.sleep(1)
+        self.moveLeft(y)
+        self.stretchForward(y)
+        time.sleep(1)
+        self.moveRight(y)
+        self.stretchBackward(y)
+
 
     def play_music(self):
         pass
