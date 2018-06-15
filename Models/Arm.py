0555995c72aef3c979da514611c8ee13a2cc5e68
Thijs <thetcr@live.nl>
2018-06-15 12:19:02 +0200

    Arm inversed kinematics

--- Models/Arm.py
+++ Models/Arm.py
@@ -20,7 +20,7 @@ class Arm3Link:
         # some default arm positions
         self.q0 = np.array([np.pi/4, np.pi/4, np.pi/4]) if q0 is None else q0
         # arm segment lengths
-        self.L = np.array([1, 1, 1]) if L is None else L
+        self.L = np.array([1, 1, 1, 1]) if L is None else L
 
         self.max_angles = [np.pi, np.pi, np.pi/4]
         self.min_angles = [0, 0, -np.pi/4]
@@ -50,14 +50,8 @@ class Arm3Link:
         return [x, y]
 
     def inv_kin(self, xy):
-        """This is just a quick write up to find the inverse kinematics
-        for a 3-link arm, using the SciPy optimize package minimization
-        function.
-
-        Given an (x,y) position of the hand, return a set of joint angles (q)
-        using constraint based minimization, constraint is to match hand (x,y),
-        minimize the distance of each joint from it's default position (q0).
-
+        """
+        find the inverse kinematics for a 3 link arm with there joints to the desired position
         xy : tuple
             the desired xy position of the arm
 
@@ -66,14 +60,8 @@ class Arm3Link:
         """
 
         def distance_to_default(q, *args):
-            """Objective function to minimize
-            Calculates the euclidean distance through joint space to the
-            default arm configuration. The weight list allows the penalty of
-            each joint being away from the resting position to be scaled
-            differently, such that the arm tries to stay closer to resting
-            state more for higher weighted joints than those with a lower
-            weight.
-
+            """
+            Calculate the distance between the joint space and the start position
             q : np.array
                 the list of current joint angles
 
@@ -87,9 +75,8 @@ class Arm3Link:
                            for qi, q0i, wi in zip(q, self.q0, weight)]))
 
         def x_constraint(q, xy):
-            """Returns the corresponding hand xy coordinates for
-            a given set of joint angle values [shoulder, elbow, wrist],
-            and the above defined arm segment lengths, L
+            """
+            Return corresponding constraint for an arm segment
 
             q : np.array
                 the list of current joint angles
@@ -104,9 +91,8 @@ class Arm3Link:
             return x
 
         def y_constraint(q, xy):
-            """Returns the corresponding hand xy coordinates for
-            a given set of joint angle values [shoulder, elbow, wrist],
-            and the above defined arm segment lengths, L
+            """
+            Return corresponding constraint for an arm segment
 
             q : np.array
                 the list of current joint angles
@@ -159,50 +145,5 @@ class Arm3Link:
             iprint=0)  # iprint=0 suppresses output
 
 
-def test():
-    # ###########Test it!##################
-
-    arm = Arm3Link()
-
-    # set of desired (x,y) hand positions
-    x = np.arange(-.75, .75, .05)
-    y = np.arange(.25, .75, .05)
-
-    # threshold for printing out information, to find trouble spots
-    thresh = .025
-
-    count = 0
-    total_error = 0
-    # test it across the range of specified x and y values
-    for xi in range(len(x)):
-        for yi in range(len(y)):
-            # test the inv_kin function on a range of different targets
-            xy = [x[xi], y[yi]]
-            # run the inv_kin function, get the optimal joint angles
-            q = arm.inv_kin(xy=xy)
-            # find the (x,y) position of the hand given these angles
-            actual_xy = arm.get_xy(q)
-            # calculate the root squared error
-            error = np.sqrt(np.sum((np.array(xy) - np.array(actual_xy))**2))
-            # total the error
-            total_error += np.nan_to_num(error)
-
-            # if the error was high, print out more information
-            if np.sum(error) > thresh:
-                print('-------------------------')
-                print('Initial joint angles', arm.q)
-                print('Final joint angles: ', q)
-                print('Desired hand position: ', xy)
-                print('Actual hand position: ', actual_xy)
-                print('Error: ', error)
-                print('-------------------------')
-
-            count += 1
-
-    print('\n---------Results---------')
-    print('Total number of trials: ', count)
-    print('Total error: ', total_error)
-    print('-------------------------')
-
 if __name__ == '__main__':
-    test()
+    Arm3Link()
