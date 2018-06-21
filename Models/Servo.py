2840caa5847d4b3ea6ea52e51888b1f1b03833a1
arlanschouwstra <scho1514@student.nhl.nl>
2018-06-21 14:11:27 +0200

    servo with json

--- Models/Servo.py
+++ Models/Servo.py
@@ -60,7 +60,7 @@ class Servo:
                            512,
                            50)
         self.move_position(51,
-                           812,
+                           512,
                            50)
 
         #already in the unit test. We should run the test on every boot.
@@ -75,62 +75,32 @@ class Servo:
 
     # old method to determine speed of the sensitivity for the joystick
     @staticmethod
-    def get_speed(joystick):
-        # ______to the right________#
-        if joystick == 1:
-            return 200
-        if joystick == 2:
-            return 150
-        if joystick == 3:
-            return 100
-        if joystick == 4:
-            return 50
-        # _______to the left_________#
-        if joystick == 6:
-            return 50
-        if joystick == 7:
-            return 100
-        if joystick == 8:
-            return 150
-        if joystick == 9:
-            return 200
+    def get_speed(joystick, data):
+        pass
 
     # moving the given servo forward or backward on button press
     # body, clockwise and vertical are booleans used for easy initialisations for the servos positions
-    def move(self, data, servo_id, start_position, clockwise, body, vertical):
+    def move(self, data, servo_id, start_position, clockwise, body):
         # initialize each position of the joystick
-        data02 = int(data[0:2])
-        data24 = int(data[2:4])
-        data46 = int(data[4:6])
-        data68 = int(data[6:8])
-        try:
-            if 25 > data68 > 20:  # move up body
-                self.move_position(servo_id,
-                                   start_position,
-                                   50)
+        if 15 > data > 10:
+            if clockwise:
+                if body:
+                    self.move_position(servo_id, start_position, 50)
+                else:
+                    self.move_position(servo_id, start_position - 300, 50)
 
-            self.move_position(servo_id,
-                               start_position(+ 200 if data68 > 25 and not clockwise else - 200),
-                               50)
+            if not clockwise:
+                if body:
+                    self.move_position(servo_id, start_position, 50)
+                else:
+                    self.move_position(servo_id, start_position + 300, 50)
 
-            self.move_position(servo_id,
-                               start_position(+ 200 if data46 < 15 else - 200),
-                               50)
+        if 15 < data < 20:
+            if clockwise:
+                self.move_position(servo_id, start_position + 300, 50)
 
-            self.move_position(servo_id,
-                               start_position(+ 200 if data02 < 35 else - 200),
-                               50)
-
-            self.move_position(servo_id,
-                               start_position(+ 200 if data24 < 45 else - 200),
-                               50)
-            print("Servo:",
-                  servo_id,
-                  "moved to position:",
-                  self.y.readPosition(servo_id))
-
-        except ValueError:
-            print("Could not turn servo!!")
+            if not clockwise:
+                self.move_position(servo_id, start_position - 300, 50)
 
     def stop_all_servos(self):
         self.move_position(3,
@@ -156,21 +126,28 @@ class Servo:
                            1)
         time.sleep(0.01)
 
-
-    def parse_data(self, data):
-        pass
     def move_all_servos(self, data):
         # initialize servos
-        start_positions = [[3, 512, True, True, True],  # right under for up/down
-                           [4, 512, False, False, False],  # top servo
-                           [6, 200, False, False, False],
-                           [15, 512, False, True, True],
-                           [23, 512, False, True, True],
-                           [41, 512, True, True, False],  # left under for up/down
-                           [51, 812, True, False, False]]
+        # start_positions = [[3, 512, True, True, True],  # right under for up/down
+        #                    [4, 512, False, False, False],  # top servo
+        #                    [6, 200, False, False, False],
+        #                    [15, 512, False, True, True],
+        #                    [23, 512, False, True, True],
+        #                    [41, 512, True, True, False],  # left under for up/down
+        #                    [51, 812, True, False, False]]
 
-        for servos in start_positions:
-            self.move(data, servos[0], servos[1], servos[2], servos[3], servos[4])
+        if 'lx' in data:
+            self.move(data["lx"], 41, 512, True, False)
+        if 'ly' in data:
+            self.move(data["ly"], 3, 512, True, True)
+            self.move(data["ly"], 15, 512, False, True)
+            self.move(data["ly"], 23, 512, False, True)
+        if 'rx' in data:
+            self.move(data["rx"], 6, 200, False, False)
+        if 'ry' in data:
+            self.move(data["ry"], 51, 512, True, False)
+
+            # self.move(data, servos[0], servos[1], servos[2], servos[3], servos[4])
 
     def move_position(self, servo_id, position, speed):  # for easy usage in other classes
         self.y.moveSpeed(servo_id, position, speed)
