c57ef60c55f6ea6efe275a18e0f64cf4ae48a09d
Thijs <thetcr@live.nl>
2018-06-26 15:41:50 +0200

    install git as well

--- Raspberry/packages.sh
+++ Raspberry/packages.sh
@@ -3,6 +3,7 @@
 sudo /usr/bin/apt-get update
 sudo /usr/bin/apt-get upgrade -y
 sudo /usr/bin/apt-get install python3 -y
+sudo /usr/bin/apt-get install git -y
 pip install numpy
 pip install scipy
 pip install imutils
