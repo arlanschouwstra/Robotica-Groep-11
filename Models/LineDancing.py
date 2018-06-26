4da335ced1f5365642505e74922c688d84e6c722
arlanschouwstra <scho1514@student.nhl.nl>
2018-06-26 16:37:51 +0200

    LineDancing while loop

--- Models/LineDancing.py
+++ Models/LineDancing.py
@@ -1,6 +1,6 @@
 import serial
 from Servo import Servo
-
+import time
 
 class LineDance():
     def __init__(self):
@@ -8,23 +8,23 @@ class LineDance():
         self.move = getattr(self.servo, 'move_position')
         self.read_pos = getattr(self.servo, 'read_position')
         self.ser = serial.Serial('/dev/ttyUSB0', 9600)
+        self.detect()
+
     def detect(self):
-        if self.ser.inWaiting() > 0:
-            inputValue = self.ser.readline()
-            print(inputValue)
-            if inputValue.startswith('{beat: 8}') or inputValue.startswith('{beat: 7}'):
-                print('Bo0M!!')
-                if self.read_pos(1) >= 805:
-                    self.move_serv(1, 612, 200)
-                    self.detect()
-                if self.read_pos(1) <= 614:
-                    self.move_serv(1, 812, 200)
-                    self.detect()
-        else:
-            self.detect()
+        while True:
+            if self.ser.inWaiting() > 0:
+                inputValue = self.ser.readline()
+                print(inputValue)
+                if inputValue.startswith('{beat: 8}') or inputValue.startswith('{beat: 7}'):
+                    print('Bo0M!!')
+                    if self.read_pos(1) >= 805:
+                        self.move_serv(1, 612, 200)
+
+                    if self.read_pos(1) <= 614:
+                        self.move_serv(1, 812, 200)
+                time.sleep(0.5)
     def move_serv(self, servo_id, position, speed):
         return self.move(servo_id, position, speed)
 
 
 linedance = LineDance()
-linedance.detect()
