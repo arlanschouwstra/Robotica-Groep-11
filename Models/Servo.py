17cac9527c3cc3f94f5a8ab11741cb5e546c85c1
arlanschouwstra <scho1514@student.nhl.nl>
2018-06-27 09:24:07 +0200

    servo code

--- Models/Servo.py
+++ Models/Servo.py
@@ -1,23 +1,19 @@
 '''
 Date:           03-08-2018
 Creator:        Arlan Schouwstra
-Edited by:      Thijs Zijlstra
 Version:        3.2
 Description:    Servos
 '''
 
-import math
-from pyax12.connection import Connection
+import ax12 as x
 import time
 
 
 class Servo:
     """_______VARIABLES__________"""
-    # y = x.Ax12()
+    y = x.Ax12()
 
     def __init__(self):
-        self.time = time
-        self.serial_connection = Connection(port="/dev/ttyAMA0", rpi_gpio=True)
         pass
 
     # important! make sure the result position is not less than 0
@@ -26,17 +22,25 @@ class Servo:
                     start_position,
                     degree,
                     clockwise):
-        if clockwise:
-            self.serial_connection.goto(servo, (start_position - (312 if degree == 90 else 312 / (90 / degree))), speed=512,
-                                   degrees=True)
-
+        if degree == 90:
+            if clockwise:
+                self.y.moveSpeed(servo,
+                                 (start_position - 312),
+                                 50)
+            else:
+                self.y.moveSpeed(servo,
+                                 (start_position + 312),
+                                 50)
         else:
-            self.serial_connection.goto(servo, start_position + (312 if degree == 90 else 312 / (90 / degree)), speed=512,
-                                   degrees=True)
-
-            # self.move_position(servo,
-            #                    (start_position + (312 if degree == 90 else 312 / (90 / degree))),
-            #                    50)
+            get_degree = 90 / degree
+            if clockwise:
+                self.y.moveSpeed(servo,
+                                 (start_position - (312 / get_degree)),
+                                 50)
+            else:
+                self.y.moveSpeed(servo,
+                                 (start_position - (312 / get_degree)),
+                                 50)
 
         print("moving servo: ",
               servo,
@@ -46,90 +50,179 @@ class Servo:
 
     def reset_servos(self):
         # set servos to their start positions
-        self.serial_connection.goto(3, 180, speed=50,
-                                    degrees=True)
-        self.serial_connection.goto(4, 180, speed=50,
-                                    degrees=True)
-        self.serial_connection.goto(6, 180, speed=50,
-                                    degrees=True)
-        self.serial_connection.goto(15, 180, speed=50,
-                                    degrees=True)
-        self.serial_connection.goto(23, 180, speed=50,
-                                    degrees=True)
-        self.serial_connection.goto(41, 180, speed=50,
-                                    degrees=True)
-        self.serial_connection.goto(51, 180, speed=50,
-                                    degrees=True)
+        self.y.moveSpeed(7,  # id
+                         512,  # position
+                         50)  # speed
+        self.y.moveSpeed(6,
+                         512,
+                         50)
+        self.y.moveSpeed(9,
+                         200,
+                         50)
+        self.y.moveSpeed(15,
+                         512,
+                         50)
+        self.y.moveSpeed(1,
+                         512,
+                         50)
+
+        # print servos positions (for testing purposes)
+        print(self.y.readPosition(7))
+        print(self.y.readPosition(9))
+        print(self.y.readPosition(6))
+        print(self.y.readPosition(1))
+
+    # old method to determine speed of the sensitivity for the joystick
+    @staticmethod
+    def get_speed(joystick):
+        # ______to the right________#
+        if joystick == 1:
+            return 200
+        if joystick == 2:
+            return 150
+        if joystick == 3:
+            return 100
+        if joystick == 4:
+            return 50
+        # _______to the left_________#
+        if joystick == 6:
+            return 50
+        if joystick == 7:
+            return 100
+        if joystick == 8:
+            return 150
+        if joystick == 9:
+            return 200
 
     # moving the given servo forward or backward on button press
     # body, clockwise and vertical are booleans used for easy initialisations for the servos positions
-    def move(self, data, servo_id, start_position, clockwise, body):
+    def move(self, data, servo_id, start_position, clockwise, body, vertical):
         # initialize each position of the joystick
-        if 15 > data > 10:
-            if clockwise:
+        try:
+            if 45 > int(data[2:4]) > 40:  # move up body
                 if body:
-                    self.move_position(servo_id, start_position, 50)
-                else:
-                    self.move_position(servo_id, start_position - 300, 50)
+                    if vertical:
+                        self.y.moveSpeed(servo_id,
+                                         start_position,
+                                         50)
+                        print("Servo:",
+                              servo_id,
+                              "moved to position:",
+                              self.y.readPosition(servo_id))
 
-            if not clockwise:
+            elif 45 < int(data[2:4]) < 50:  # move down body
                 if body:
-                    self.move_position(servo_id, start_position, 50)
-                else:
-                    self.move_position(servo_id, start_position + 300, 50)
+                    if vertical:
+                        if not clockwise:
+                            self.y.moveSpeed(servo_id,
+                                             start_position + 300,
+                                             50)
+                            print("Servo:",
+                                  servo_id,
+                                  "moved to position:",
+                                  self.y.readPosition(servo_id))
+                        else:
+                            self.y.moveSpeed(servo_id,
+                                             start_position - 300,
+                                             50)
+                            print("Servo:",
+                                  servo_id,
+                                  "moved to position:",
+                                  self.y.readPosition(servo_id))
 
-        if 15 < data < 20:
-            if clockwise:
-                self.move_position(servo_id, start_position + 300, 50)
+            elif 15 > int(data[4:6]) > 10:  # move left body
+                if body:
+                    if not vertical:
+                        self.y.moveSpeed(servo_id,
+                                         start_position + 300,
+                                         50)
+                        print("Servo:",
+                              servo_id,
+                              "moved to position:",
+                              self.y.readPosition(servo_id))
 
-            if not clockwise:
-                self.move_position(servo_id, start_position - 300, 50)
+            elif 15 < int(data[4:6]) < 20:  # move right body
+                if body:
+                    if not vertical:
+                        self.y.moveSpeed(servo_id,
+                                         start_position - 300,
+                                         50)
+                        print("Servo:",
+                              servo_id,
+                              "moved to position:",
+                              self.y.readPosition(servo_id))
+
+            elif 35 > int(data[0:2]) > 30:  # move left head
+                if not body:
+                    if not vertical:
+                        if clockwise:
+                            self.y.moveSpeed(servo_id,
+                                             start_position + 300,
+                                             50)
+                            print("Servo:",
+                                  servo_id,
+                                  "moved to position:",
+                                  self.y.readPosition(servo_id))
+
+            elif 35 < int(data[0:2]) < 40:  # move right head
+                if not body:
+                    if not vertical:
+                        if clockwise:
+                            self.y.moveSpeed(servo_id,
+                                             start_position - 300,
+                                             50)
+                            print("Servo:",
+                                  servo_id,
+                                  "moved to position:",
+                                  self.y.readPosition(servo_id))
+
+            elif 45 > int(data[2:4]) > 40:  # move up head
+                if not body:
+                    if not vertical:
+                        if not clockwise:
+                            self.y.moveSpeed(servo_id,
+                                             start_position + 300,
+                                             50)
+                            print("Servo:",
+                                  servo_id,
+                                  "moved to position:",
+                                  self.y.readPosition(servo_id))
+
+            elif 45 < int(data[2:4]) < 50:  # move down head
+                if not body:
+                    if not vertical:
+                        if not clockwise:
+                            self.y.moveSpeed(servo_id,
+                                             start_position - 300,
+                                             50)
+                            print("Servo:",
+                                  servo_id,
+                                  "moved to position:",
+                                  self.y.readPosition(servo_id))
+
+        except ValueError:
+            print("Could not turn servo!!")
 
     def stop_all_servos(self):
-        self.move_position(3,
-                           int(self.read_position(3)),
-                           1)
-        self.move_position(4,
-                           int(self.read_position(4)),
-                           1)
-        self.move_position(6,
-                           int(self.read_position(6)),
-                           1)
-        self.move_position(15,
-                           int(self.read_position(15)),
-                           1)
-        self.move_position(23,
-                           int(self.read_position(23)),
-                           1)
-        self.move_position(41,
-                           int(self.read_position(41)),
-                           1)
-        self.move_position(51,
-                           int(self.read_position(51)),
-                           1)
+        self.y.moveSpeed(7,
+                         int(self.y.readPosition(7)),
+                         1)
+        self.y.moveSpeed(1,
+                         int(self.y.readPosition(1)),
+                         1)
         time.sleep(0.01)
 
-    def move_all_servos(self, key, data):
-        if 'lx' == key:
-            self.move(data, 41, 512, True, False)
-        if 'ly' == key:
-            self.move(data, 3, 512, True, True)
-            self.move(data, 15, 512, False, True)
-            self.move(data, 23, 512, False, True)
-        if 'rx' == key:
-            self.move(data, 6, 200, False, False)
-        if 'ry' == key:
-            self.move(data, 51, 512, True, False)
+    # used to call all servos
+    def move_all_servos(self, data):
+        # initialize servos
+        start_positions = [[7, 512, False, True, True],  # right under for up/down
+                           [1, 512, False, True, True]]
 
-            # self.move(data, servos[0], servos[1], servos[2], servos[3], servos[4])
+        for servos in start_positions:
+            self.move(data, servos[0], servos[1], servos[2], servos[3], servos[4])
 
     def move_position(self, servo_id, position, speed):  # for easy usage in other classes
-
-        self.serial_connection.goto(servo_id,  math.floor(position/2.844444), speed=speed,
-                                    degrees=True)
-        # self.y.moveSpeed(servo_id, position, speed)
+        self.y.moveSpeed(servo_id, position, speed)
 
     def read_position(self, servo_id):  # for easy usage in other classes
-        return self.serial_connection.present_position(servo_id)
-        # return self.y.readPosition(servo_id)
-
+        return self.y.readPosition(servo_id)
